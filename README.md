ddef65c78852545e5de8bc256b078c2543784ca2
Ralph <mail@ralphschmeisser.de>
2023-12-10 10:32:10 +0100

    first version 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# telekom-negativliste-updater-
-Telekom Negativliste Updater: A Python script that automates the process of updating the Telekom Negativliste with the most active spam numbers in Germany, blocking unwanted calls.
+# Telekom Negativliste Updater 
+ 
+## Background 
+ 
+My mother has been receiving a significant number of spam calls lately, and the built-in features of the Speedport router are not effective in blocking these calls. To address this issue, I have developed a Python script,  telekom_negativliste_updater.py , that scrapes a website listing the most active spam numbers in Germany for the last 24 hours. The script then updates the Telekom Negativliste, which is an online portal provided by Telekom, to block these spam numbers. Additionally, I have configured the Telefoniecenter to disallow unknown numbers from calling my mother altogether. 
+
+## Status
+
+Just a proof of concept. Currently it seems to work and to block most SPAM calls.
+ 
+## Prerequisites 
+ 
+- Python 3.9 or higher 
+- Firefox web browser 
+- Geckodriver for Firefox 
+ 
+## Installation 
+ 
+1. Clone this repository:
+```shell
+git clone https://github.com/rsmuc/telekom-negativliste-updater
+```
+2. Install the required Python packages:
+```shell
+pip install -r requirements.txt
+```
+3. Download and install Geckodriver for Firefox. You can find the latest version [here](https://github.com/mozilla/geckodriver/releases). 
+
+## Installation of Geckodriver:
+
+1. Visit the Geckodriver releases page on GitHub: [https://github.com/mozilla/geckodriver/releases](https://github.com/mozilla/geckodriver/releases) 
+ 
+2. Scroll down to the latest release and find the appropriate version for your operating system. Geckodriver is available for Windows, macOS, and Linux. 
+ 
+3. Download the Geckodriver executable file for your operating system. 
+ 
+4. Extract the contents of the downloaded archive. 
+ 
+5. Add the location of the extracted Geckodriver executable to your system's  PATH  environment variable. This step allows you to run Geckodriver from any directory in your command prompt or terminal. 
+ 
+6. Verify the installation by opening a command prompt or terminal window and running the following command:
+```shell
+   geckodriver --version
+```
+If the installation was successful, you should see the version number of Geckodriver printed in the console. 
+ 
+Please note that the exact steps for installing Geckodriver may vary depending on your operating system. Make sure to provide instructions specific to the operating system you are targeting in your README. 
+ 
+Remember to include any additional details or troubleshooting steps that may be relevant to your specific use case.
+ 
+## Usage 
+ 
+The script supports the following command-line arguments: 
+ 
+-  --username : Your Telekom username. 
+-  --password : Your Telekom password. 
+-  --phone_number : The phone number where the Negativliste shall be updated. As written in the dropdown when you login to the Telefoniecenter. e.g. "089 55555"
+-  --number_source : The URL of the SPAM number source. e.g. notsocleverdialer.de 
+-  --simulate : Enable simulation mode to simulate adding numbers without actually modifying the Negativliste. 
+-  --interactive : Enable interactive mode to run firefox in foreground. 
+ 
+To run the script, use the following command:
+```shell
+python telekom_negativliste_updater.py --username your_username --password your_password --phone_number your_phone_number --number_source notsocleverdialer.de
+```
+The script will log in to the Telekom Telefoniecenter, navigate to the Negativliste, and update it with the current most active spam numbers from the specified source. 
+
+Please note that the specific source of the spam number list is not directly mentioned in this README, as scraping data from certain sources may not be allowed. You can configure the  --number_source  argument with a valid URL of a spam number source that complies with the terms of use and legal requirements. 
+ 
+## Limitations 
+ 
+- The Telekom Negativliste allows blocking up to 50 numbers. If the number of spam numbers exceeds this limit, only the first 50 will be blocked. 
+- The script relies on the availability and accuracy of the number source website. If the website structure or content changes, the script may need to be updated accordingly. 
+- Changes to the Telekom page structure or functionality may cause the script to stop working. Regular maintenance and updates may be required to keep the script functioning properly. 
+ 
+## License 
+ 
+This script is licensed under the [MIT License](LICENSE). 
---
+++ telekom_negativliste_updater.py
@@ -0,0 +1,187 @@
+# Telekom Negativliste Updater - Version 0.0.1
+import argparse
+import logging
+import requests
+import time
+import warnings
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from systemd.journal import JournalHandler
+
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+log = logging.getLogger('demo')
+log.addHandler(JournalHandler())
+log_formatter = logging.Formatter('%(levelname)s: %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(log_formatter)
+log.addHandler(stream_handler)
+log.setLevel(logging.INFO)
+log.info("Started Telekom")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--username", type=str, help="Your Telekom username")
+parser.add_argument("--password", type=str, help="Your Telekom password")
+parser.add_argument("--phone_number", type=str, help="The phone number where the negativliste shall be updated")
+parser.add_argument("--number_source", type=str, help="The URL of the SPAM number source")
+parser.add_argument("--simulate", action="store_true", help="Enable simulation mode")
+parser.add_argument("--interactive", action="store_true", help="Enable interactive mode")
+
+
+args = parser.parse_args()
+
+username = args.username
+password = args.password
+phone_number_text = args.phone_number
+number_source1 = ("https://www.%s.de/top-spammer-der-letzten-24-stunden" % args.number_source)
+simulate_mode = args.simulate
+interactive_mode = args.interactive
+
+
+def login_to_negativliste():
+    # Open Telekom Telefoniecenter
+    driver.get("https://accounts.login.idm.telekom.com/oauth2/auth")
+
+    driver.get("https://telefoniecenter.sgp.telekom.de/fcc")
+
+    driver.set_window_size(708, 745)
+    # Login
+    driver.find_element(By.ID, "username").click()
+    driver.find_element(By.ID, "username").send_keys(username)
+    driver.find_element(By.ID, "pw_submit").click()
+    driver.find_element(By.ID, "pw_pwd").click()
+    driver.find_element(By.ID, "pw_pwd").send_keys(password)
+    driver.find_element(By.ID, "pw_submit").click()
+
+    # Select the number where the Negativliste shall be updated
+    driver.find_element(By.LINK_TEXT, "Rufnummer auswählen").click()
+    phone_number_xpath = f".//li[text()='{phone_number_text}']"
+    # Wait for the phone number to be clickable (if needed)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, phone_number_xpath)))
+    # Click on the phone number
+    driver.find_element(By.XPATH, phone_number_xpath).click()
+    driver.find_element(By.ID, "intro-continue-button").click()
+
+    # Open Negativliste
+    driver.get("https://telefoniecenter.sgp.telekom.de/fcc/view/availabilitycallblocking")
+    driver.find_element(By.CSS_SELECTOR, ".btn:nth-child(2)").click()
+    driver.execute_script("window.scrollBy(0,1000)")
+    time.sleep(1)
+    driver.find_element(By.LINK_TEXT, "Negativliste").click()
+    driver.set_window_size(550, 691)
+
+
+def add_number(phone_number):
+    if simulate_mode:
+        log.info("Simulating add_number for phone number: %s", phone_number)
+        return
+    try:
+        log.info("insert number")
+        driver.find_element(By.CSS_SELECTOR, ".add-number").click()
+        driver.find_element(By.CSS_SELECTOR, ".add-number").send_keys(phone_number)
+        driver.find_element(By.CSS_SELECTOR, ".add-item").click()
+        time.sleep(2)
+        log.info("clean input field")
+        # cleanup input field if something went wrong
+        driver.find_element(By.CSS_SELECTOR, ".add-number").click()
+        driver.find_element(By.CSS_SELECTOR, ".add-number").clear()
+        driver.find_element(By.CSS_SELECTOR, ".list-header").click()
+    except:
+        log.error("failed to add number")
+        time.sleep(5)
+
+
+def delete_number():
+    if simulate_mode:
+        log.info("Simulating delete_number")
+        return
+    driver.find_element(By.CSS_SELECTOR, "div:nth-child(1) > .tooltip .ui-icon-only-button").click()
+    time.sleep(3)
+
+def main():
+    log.info("Update Telekom Negativliste started")
+
+    log.info("CLEAN LIST")
+    errors = 0
+    for i in range(55):
+        try:
+            delete_number()
+            log.info("Deleted entry")
+        except:
+            amount = driver.find_element(By.CSS_SELECTOR, ".no-entries").text
+            log.info("amount is " + amount)
+            if amount == "Keine":
+                log.info("Not deleted - Empty! - Break")
+                break
+            errors = errors + 1
+            log.info("Not deleted - Empty?")
+            if errors > 50:
+                break
+
+    log.info("Block +31")
+    add_number("+31")
+
+    log.info("Started with TOP SPAM")
+
+    page = requests.get(number_source1)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    title = soup.title.text
+    links = soup.select('a')
+    for i, link in enumerate(links):
+        if link.text.startswith("0"):
+            number = link.text
+            add_number(number)
+            log.info("added top spam number " + str(i) + " " + number)
+
+        if i > 55:
+            break
+
+    # run he
+if interactive_mode:
+    # Running on Fedora with GNOME
+    driver = webdriver.Firefox()
+
+else:
+    # Running on Debian headless
+    options = FirefoxOptions()
+    options.add_argument("--headless")
+    service = Service()
+    driver = webdriver.Firefox(service=service, options=options)
+
+driver.implicitly_wait(100)
+
+try:
+
+    login_to_negativliste()
+
+    main()
+
+
+    # check amount
+    amount = driver.find_element(By.CSS_SELECTOR, ".amount").text
+    log.info("amount is " + amount)
+
+    if amount != "50":
+        # retry if there are too less entries. won't try it a third time...
+        log.error("amount is too low. try again.")
+        main()
+
+    # enable blocklist
+    try:
+        log.info("activate negativliste")
+        driver.find_element(By.CSS_SELECTOR, ".secondary").click()
+        driver.find_element(By.CSS_SELECTOR, ".black-list .border").click()
+    except:
+        log.error("activate negativliste FAILED")
+
+finally:
+    # end selenium
+    driver.quit()
+    log.info("finished")
+    exit()
